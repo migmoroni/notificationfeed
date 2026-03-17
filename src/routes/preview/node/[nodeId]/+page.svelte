7e5e8fe7461d67dbeecb03470f62300c12e42afc
Miguel Moroni <miguelmcontato@gmail.com>
2026-03-17 10:23:48 -0300

    fix: routes for browse and favorites and preview

---
+++ src/routes/preview/node/[nodeId]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import NodeDetailPage from '$lib/components/shared/entity/NodeDetailPage.svelte';
+
+	const nodeId = $derived(page.params.nodeId!);
+</script>
+
+<NodeDetailPage {nodeId} baseHref="/preview" />
