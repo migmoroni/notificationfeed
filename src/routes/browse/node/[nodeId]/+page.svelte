e4479eb177c0d8b3bbb6757c974b5179dbbba6d0
Miguel Moroni <miguelmcontato@gmail.com>
2026-04-25 01:08:58 -0300

    fix: internal node

--- src/routes/browse/node/[nodeId]/+page.svelte
+++ src/routes/browse/node/[nodeId]/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import { onMount } from 'svelte';
 	import NodeDetailPage from '$lib/components/shared/entity/NodeDetailPage.svelte';
 	import { activityService } from '$lib/services/activity.service.js';
 
 	const nodeId = $derived(page.params.nodeId!);
 
-	onMount(() => {
+	$effect(() => {
 		void activityService.record({ type: 'open', targetType: 'node', targetId: nodeId, context: 'browse' });
 	});
 </script>
